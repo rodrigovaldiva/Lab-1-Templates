82da7ea5b003e9bcd68cc310f42f775efc0ef7f4
sqalli <mts517@nyu.edu>
2023-09-01 18:38:19 +0400

    Create lab1exercise4a.cpp

---
+++ lab1exercise4a.cpp
@@ -0,0 +1,27 @@
+/*4A: Write an inline function Min(int,int) to calculate minimum between 
+two numbers.*/
+
+
+#include <iostream>
+
+// Inline function to calculate the minimum of two integers
+inline int Min(int a, int b) {
+    // It takes two integers as input parameters (a and b) and returns the minimum of the two using the conditional (ternary) operator ? :.
+    
+}
+
+int main() {
+    int num1, num2;
+    
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+    int minimum =/*Call the Min function with the first and second number*/;
+
+    std::cout << "The minimum between " << num1 << " and " << num2 << " is " << minimum << std::endl;
+
+    return 0;
+}
