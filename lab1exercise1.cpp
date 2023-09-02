e88b4741339254ea20477fce6fda1e3198dc0c42
sqalli <mts517@nyu.edu>
2023-09-02 14:22:26 +0400

    Created lab1exercise1.cpp

--- lab1exercise1.cpp
+++ lab1exercise1.cpp
@@ -3,61 +3,59 @@ an array of an arbitrary size (specified by the user). Then the user may extend 
 to a bigger size (this involves creating a temporary array with larger size, copying the 
 values of the elements, and adding new values into the extension cells). Print the new 
 array to the user to confirm the functionality.*/
+
 #include <iostream>
 
-using namespace std;
+int* resizeArray(int* oldArray, int oldSize, int newSize) {
+    // (1) Allocate memory for the initial array with the name newArray with the specified newSize
+    
+   
+    for (int i = 0; i < oldSize; ++i) {
+         // (2) Copy elements from the old array to the new array
+    }
 
-template <typename T>
-void printArray(T arr, int size){
-    for(int i = 0; i < size; i++) cout << arr[i] << ' ';
-    cout << '\n';
+    // Initialize new elements in the extended part of the array
+    for (int i = oldSize; i < newSize; ++i) {
+        std::cout << "Enter a new value for element " << i << ": ";
+        std::cin >> newArray[i];
+    }
+
+    // (3) Deallocate memory for the old array
+
+    return newArray;
 }
 
-int main(int argc, char *argv[]){
-    // fun fact: argc stands for ARGument Count
-    // and argv stands for ARGument Vector
+int main() {
+    int* array = nullptr;
+    int size, newSize;
 
-    //Declare an integer variable that will be used to store the size of the array
+    std::cout << "Enter the initial size of the array: ";
+    std::cin >> size;
 
-    cout << "Give me initial size of array\n";
-    // Get the size of the array from the user and save it in the variable you declared
+    // (4) Allocate memory for the initial array with the size inputted by the user
 
-    
-    // The line below declares a pointer variable named array that can store the memory address of the beginning of the array.
-    // new char[size]: This is a dynamic memory allocation statement. 
-    // It allocates a block of memory on the heap (i.e., the memory managed by the program during runtime) 
-    // to store an array of characters. 
-    char *array = new char[size];
+    std::cout << "Enter values for the initial array:" << std::endl;
+    for (int i = 0; i < size; ++i) {
+        std::cout << "Element " << i << ": ";
+        std::cin >> array[i];
+    }
 
-    // Write a for loop that will initialize the array with the English Alphabet: A, B, C, D ... until the size that the user
-    // specified is reached 
-    
+    std::cout << "Enter the new size of the array (must be greater than the current size): ";
+    std::cin >> newSize;
 
-    //int newSize;
-    cout << "Give me new size for the array\n";
-    // Get the new size of the array from the user and save it in the variable you declared
+    if (newSize <= size) {
+        std::cout << "New size must be greater than the current size." << std::endl;
+    } else {
+        int* resizedArray = resizeArray(array, size, newSize);
 
-    //declare a temporary pointer variable named temp to be used to copy elements from the previously declared array 
-    char *temp = new char[newSize];
+        std::cout << "Array after resizing to size " << newSize << ":" << std::endl;
+        for (int i = 0; i < newSize; ++i) {
+            std::cout << "Element " << i << ": " << resizedArray[i] << std::endl;
+        }
 
-    // Use a for loop to copy the already existing elements of the first array into the temporary array
-    // note we only go until size since we only have 'size' number of elements
-
-    // add the extra characters to the temporary array according to the newsize that the user has provided
-    
-    
-    // Use the delete [] function in c++ to deallocate memory for old array since we no longer have any need of it.
-   
-    /*note that we must do this before we transfer the pointer otherwise, we have
-    no way of keeping track of where the old memory is*/
-
-    // now, set the array pointer to be equal to temp (because we want our original array to be resized)
-    
-
-    // for good measure
-    temp = nullptr;
-
-    //using the printArray function above, print the elements of the 'array' after resizing it
+        // (5) Deallocate memory for the resized array
+    }
 
     return 0;
 }
+
