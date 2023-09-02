29de5608856b23d88e1db954c028c7a7aad3a3be
sqalli <mts517@nyu.edu>
2023-09-02 14:54:51 +0400

    Update and rename lab1exercise6b.cpp to lab1exercise7.cpp

--- lab1exercise7.cpp
+++ lab1exercise7.cpp
@@ -14,7 +14,6 @@ void sort(/* Pass the array of type T */, size_t size) {
     for (size_t i = 0; i < size - 1; ++i) {
         for (size_t j = 0; j < size - i - 1; ++j) {
             if (arr[j] > arr[j + 1]) {
-                // Swap arr[j] and arr[j + 1]
                 T temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
